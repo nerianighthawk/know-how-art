33ba5eabcf739a60616140f808db44b102442439
夜鷹　ねりあ <hiroya.nakakawaji@gmail.com>
2019-02-26 08:08:46 +0900

    fix editor layout

--- client-app/src/components/editor.tsx
+++ client-app/src/components/editor.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import * as marked from 'marked';
 
@@ -9,8 +10,14 @@ class Editor extends React.Component {
     const multiline = true
     return (
       <div>
-      <TextField multiline={multiline} onChange={this.hundleChange}/>
+      <Grid container={true} spacing={24}>
+      <Grid item={true} xs={6}>
+      <TextField multiline={multiline} variant="outlined" rows="30" onChange={this.hundleChange}/>
+      </Grid>
+      <Grid item={true} xs={6}>
       <div id="preview"/>
+      </Grid>
+      </Grid>
       </div>
     )
   }
