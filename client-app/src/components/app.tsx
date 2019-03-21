966d4ed9d3ab99cfc21774bdec3ac954ca830f06
夜鷹　ねりあ <hiroya.nakakawaji@gmail.com>
2019-03-22 06:25:42 +0900

    login check

--- client-app/src/components/app.tsx
+++ client-app/src/components/app.tsx
@@ -5,13 +5,36 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Editor from '../components/editor';
 import TitleBar from '../views/title-bar';
 
-const App = () => (
-  <Router>
-    <div>
-      <TitleBar />
-      <Route path='/editor' component={Editor}/>
-    </div>
-  </Router>
-)
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+const App = () => {
+  const data = {
+    ClientId: '122koppabv8dql25kskc2kc2ot',
+    UserPoolId: 'us-east-1_BcdBiPzEs',
+  };
+  const userPool = new CognitoUserPool(data);
+  const cognitoUser = userPool.getCurrentUser();
+
+  console.log(cognitoUser);
+
+  if (cognitoUser != null) {
+    cognitoUser.getSession((err: string, session: any) => {
+      if (err) {
+        alert(err);
+        return;
+      }
+      console.log('session validity: ' + session.isValid());
+    });
+  }
+
+  return (
+    <Router>
+      <div>
+        <TitleBar />
+        <Route path='/editor' component={Editor} />
+      </div>
+    </Router>
+  )
+}
 
 export default App;
